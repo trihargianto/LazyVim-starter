42d1bb393850d093a1973f6faf31d5de559a32b9
Tri Hargianto <trihargianto@gmail.com>
2024-01-24 13:41:17 +0700

    disable plugins

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  -- Disable unnatural search feature
+  { "folke/flash.nvim", enabled = false },
+
+  -- Disable annyoying notifications
+  { "rcarriga/nvim-notify", enabled = false },
+
+  -- Disable indent guide highlight
+  { "echasnovski/mini.indentscope", enabled = false },
+}
