713d090d603aa52ce00c22c22b0a9fc6fc8842f5
Tri Hargianto <trihargianto@gmail.com>
2024-01-25 21:01:07 +0700

    disable dashboard on startup

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,9 +2,12 @@ return {
   -- Disable unnatural search feature
   { "folke/flash.nvim", enabled = false },
 
-  -- Disable annyoying notifications
-  { "rcarriga/nvim-notify", enabled = false },
-
   -- Disable indent guide highlight
   { "echasnovski/mini.indentscope", enabled = false },
+
+  -- Disable the startup dashboard
+  {
+    "nvimdev/dashboard-nvim",
+    enabled = false,
+  },
 }
