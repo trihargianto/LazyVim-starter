c1a3a2d1969d5055e18939a4ebc585d700469ecb
Tri Hargianto <hargianto@mamiteam.com>
2025-03-06 22:07:03 +0700

    feat(keymaps): add <leader>fy and <leader>fY to copy buffer info

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,17 @@
 vim.keymap.set("n", "x", '"_x')
 vim.keymap.set("n", "<Leader>p", '"0p')
 vim.keymap.set("n", "<Leader>P", '"0P')
+
+-- Copy full path of the current buffer
+vim.keymap.set("n", "<leader>fy", function()
+  local filepath = vim.fn.expand("%:p")
+  vim.fn.setreg("+", filepath)
+  print("Copied full path: " .. filepath)
+end, { desc = "Copy full file path" })
+
+-- Copy current buffer file name
+vim.keymap.set("n", "<leader>fY", function()
+  local filename = vim.fn.expand("%:t")
+  vim.fn.setreg("+", filename)
+  print("Copied file name: " .. filename)
+end, { desc = "Copy file name" })
