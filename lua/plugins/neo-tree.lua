ce51a49580968ea256c3106224f909ebd7c156a4
Tri Hargianto <hargianto@mamiteam.com>
2025-03-10 04:59:57 +0700

    bind to cwd on neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -5,6 +5,10 @@ return {
       window = {
         position = "right",
       },
+      filesystem = {
+        -- https://github.com/LazyVim/LazyVim/issues/3088#issuecomment-2088850067
+        bind_to_cwd = true,
+      },
     },
   },
 }
